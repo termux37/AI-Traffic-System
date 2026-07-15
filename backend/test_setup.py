31c2057248d935190fd7cf5be7e72cf5155dfc73
termux37 <nithinjohnregi2@gmail.com>
2026-07-15 19:25:20 +0530

    Removed unnecessary files

--- backend/test_setup.py
+++
@@ -1,9 +0,0 @@
-import traci
-
-traci.start(["sumo-gui", "-c", "sumo/simulation.sumocfg"])
-
-print(traci.trafficlight.getIDList())
-logic = traci.trafficlight.getAllProgramLogics("J0")
-
-print(logic)
-traci.close()
